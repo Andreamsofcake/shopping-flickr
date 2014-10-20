decd974c4e0abb37f99b962c66a62bb889aa7326
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-10-20 15:17:51 -0700

    added photos to index page, built cart route

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -2,5 +2,13 @@
 
 var ShoppingFlickr = Ember.Application.create();
 
+
 // expose ShoppingFlickr globally
 window.ShoppingFlickr = ShoppingFlickr;
+
+ShoppingFlickr.Router.map(function(){
+  // we don't need a path if the place it is going
+  // is the same as the string name (aka url path)
+  this.route('cart')
+});
+
